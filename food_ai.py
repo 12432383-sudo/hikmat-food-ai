95cd66fda0da29f84ecd8c0585c006ae6f72b593
Hikmat Noun <12432383@students.liu.edu.lb>
2026-01-23 00:46:29 +0200

    Initial working version of Hikmat Food AI

--- food_ai.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import numpy as np
-
-model = joblib.load("food_ai_model.pkl")
-
-def predict_health(sugar, salt, sat_fat, palm_oil):
-    features = np.array([[sugar, salt, sat_fat, palm_oil]])
-    prediction = model.predict(features)[0]
-    return prediction
